46fd982b63e40f64fcc9abbd8a30c543146aa4de
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2021-02-22 23:56:27 -0300

    Criação da estrutura inicial do microsserviço de Companhia.

---
+++ companhia/src/test/java/br/com/airline/companhia/CompanhiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.airline.companhia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanhiaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
