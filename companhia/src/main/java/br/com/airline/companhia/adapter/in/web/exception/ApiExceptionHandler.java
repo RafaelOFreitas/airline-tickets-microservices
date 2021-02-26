4438bff3abccfd30ca99cfcd36db15e9443c7722
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2021-02-25 22:25:53 -0300

    Atividades microsserviço de Companhia: Criação de exception handler.

--- companhia/src/main/java/br/com/airline/companhia/adapter/in/web/exception/ApiExceptionHandler.java
+++ companhia/src/main/java/br/com/airline/companhia/adapter/in/web/exception/ApiExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.NonNullApi;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
