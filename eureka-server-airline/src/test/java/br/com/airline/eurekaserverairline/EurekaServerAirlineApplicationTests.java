1176e007d4fd90235ba778bc46e97068ec4bd874
Rivaildo Sousa <rivaildojunior@yahoo.com.br>
2021-02-27 21:31:21 -0300

    Servidor eureka

---
+++ eureka-server-airline/src/test/java/br/com/airline/eurekaserverairline/EurekaServerAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.airline.eurekaserverairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
