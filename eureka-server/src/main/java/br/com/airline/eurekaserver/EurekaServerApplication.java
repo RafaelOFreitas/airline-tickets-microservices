da01ca792de04920f870e0922255f8e2b2d64007
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2021-03-02 15:23:45 -0300

    Atividades microsserviço de eureka-server: Convertendo properties para yml. Renomeando package do serviço. Padronizando pom.

--- eureka-server/src/main/java/br/com/airline/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/br/com/airline/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.airline.eurekaserverairline;
+package br.com.airline.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerAirlineApplication {
+public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerAirlineApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
 
 }
