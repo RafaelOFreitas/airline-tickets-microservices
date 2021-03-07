aea3a4fbf33cf2e53d294c73f218bb29613a6d48
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2021-03-06 22:27:13 -0300

    Atividades microsserviço de voo-command: Estrutura e dependências iniciais do projeto.

---
+++ voo-command/src/main/java/br/com/airline/voocommand/VooCommandApplication.java
@@ -0,0 +1,13 @@
+package br.com.airline.voocommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VooCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VooCommandApplication.class, args);
+    }
+
+}
