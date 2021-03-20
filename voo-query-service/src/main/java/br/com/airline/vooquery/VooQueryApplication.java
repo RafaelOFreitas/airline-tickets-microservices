e2539a9028bc5e8e3c1764c9e8f75a2ffa5fd8b8
RafaelOFreitas <rafa.oliveirafreitas@hotmail.com>
2021-03-20 15:11:05 -0300

    Criando módulo do microsserviço de voo-query.

---
+++ voo-query-service/src/main/java/br/com/airline/vooquery/VooQueryApplication.java
@@ -0,0 +1,13 @@
+package br.com.airline.vooquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VooQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VooQueryApplication.class, args);
+    }
+
+}
---
+++ voo-query-service/src/test/java/br/com/airline/vooquery/VooQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.airline.vooquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VooQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
